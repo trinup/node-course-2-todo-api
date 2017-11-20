a325b21f46434772a1328b93cf147b9fa2b5b7ed
Punit Tripathi <ptripathi50@gmail.com>
2017-11-20 23:25:50 +0530

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');  //connect mongoose to mongoDB database
+mongoose.connect("mongodb://punit:punit@ds113736.mlab.com:13736/todo-api-db" || 'mongodb://localhost:27017/TodoApp');  //connect mongoose to mongoDB database
 
 
 module.exports = {
