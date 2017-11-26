16fe9fba49a2b08433f7532706c767a163e8f13e
Punit Tripathi <ptripathi50@gmail.com>
2017-11-26 23:43:14 +0530

    Add POST \users\login

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
+var bcrypt = require('bcryptjs');
 var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -123,6 +124,19 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
    res.send(req.user);
+});
+
+
+app.post('/users/login', (req, res) => {
+    const body = _.pick(req.body, ["email", "password"]);
+    
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 
